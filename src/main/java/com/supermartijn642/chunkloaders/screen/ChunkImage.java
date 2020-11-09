194f0cd90a3d4206e8595e01e21e266681c3f573
SuperMartijn624 <martijnvandenbrand@outlook.com>
2020-11-09 18:28:08 +0100

    use bytes instead of integers

--- src/main/java/com/supermartijn642/chunkloaders/screen/ChunkImage.java
+++ src/main/java/com/supermartijn642/chunkloaders/screen/ChunkImage.java
@@ -11,6 +11,7 @@ import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL12;
 
 import java.awt.*;
+import java.nio.ByteBuffer;
 
 /**
  * Created 8/19/2020 by SuperMartijn642
@@ -20,7 +21,7 @@ public class ChunkImage {
     private final World world;
     private final ChunkPos chunkPos;
     public int textureId = -1;
-    private int[] buffer = null;
+    private byte[] buffer = null;
 
     public ChunkImage(World world, ChunkPos chunkPos){
         this.world = world;
@@ -40,18 +41,20 @@ public class ChunkImage {
         GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_NEAREST);
         GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S, GL12.GL_CLAMP_TO_EDGE);
         GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, GL12.GL_CLAMP_TO_EDGE);
-        GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGB8, 16, 16, 0, GL11.GL_RGB, GL12.GL_UNSIGNED_INT, this.buffer);
+        ByteBuffer buffer = ByteBuffer.allocateDirect(this.buffer.length);
+        buffer.put(this.buffer).flip();
+        GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGB8, 16, 16, 0, GL11.GL_RGB, GL11.GL_UNSIGNED_BYTE, buffer);
     }
 
-    private int[] createBuffer(){
+    private byte[] createBuffer(){
         int width = 16;
         int height = 16;
 
-        int[] rgbArray = new int[width * height * 3];
+        byte[] rgbArray = new byte[width * height * 3];
 
         for(int x = 0; x < width; x++){
             for(int z = 0; z < height; z++){
-                BlockPos pos = this.world.getHeight(Heightmap.Type.WORLD_SURFACE, this.chunkPos.getBlock(x, 0, z)).down();
+                BlockPos pos = this.world.getHeight(Heightmap.Type.WORLD_SURFACE, new BlockPos(this.chunkPos.getXStart() + x, 0, this.chunkPos.getZStart() + z)).down();
                 int northY = this.world.getHeight(Heightmap.Type.WORLD_SURFACE, pos.getX(), pos.getZ() - 1) - 1;
                 int westY = this.world.getHeight(Heightmap.Type.WORLD_SURFACE, pos.getX() - 1, pos.getZ()) - 1;
 
@@ -66,9 +69,9 @@ public class ChunkImage {
                 rgb = color.getRGB();
 
                 int index = (x * height + z) * 3;
-                rgbArray[index] = (int)(((rgb >> 16) & 255) / 255f * Integer.MAX_VALUE);
-                rgbArray[index + 1] = (int)(((rgb >> 8) & 255) / 255f * Integer.MAX_VALUE);
-                rgbArray[index + 2] = (int)((rgb & 255) / 255f * Integer.MAX_VALUE);
+                rgbArray[index] = (byte)((rgb >> 16) & 255);
+                rgbArray[index + 1] = (byte)(double)((rgb >> 8) & 255);
+                rgbArray[index + 2] = (byte)(double)(rgb & 255);
             }
         }
 
