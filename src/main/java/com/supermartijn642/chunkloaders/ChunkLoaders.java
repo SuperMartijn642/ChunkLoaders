da26e5460874f139dd1691561240b5270981da9c
SuperMartijn624 <martijnvandenbrand@outlook.com>
2020-11-23 22:09:48 +0100

    update version

--- src/main/java/com/supermartijn642/chunkloaders/ChunkLoaders.java
+++ src/main/java/com/supermartijn642/chunkloaders/ChunkLoaders.java
@@ -23,7 +23,7 @@ public class ChunkLoaders {
     public static final String MODID = "chunkloaders";
     public static final String NAME = "Chunk Loaders";
     public static final String MC_VERSIONS = "[1.12.2]";
-    public static final String VERSION = "1.1.2";
+    public static final String VERSION = "1.1.3";
 
     public static ChunkLoaders instance;
 
