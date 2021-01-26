2fb3e80707df6e1f1dc6899b06117b6a6b26ad01
SuperMartijn624 <martijnvandenbrand@outlook.com>
2021-01-26 21:30:17 +0100

    Make the gui display custom grid sizes correctly

--- src/main/java/com/supermartijn642/chunkloaders/screen/ChunkLoaderScreen.java
+++ src/main/java/com/supermartijn642/chunkloaders/screen/ChunkLoaderScreen.java
@@ -1,19 +1,19 @@
 package com.supermartijn642.chunkloaders.screen;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
-import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.platform.GlStateManager;
 import com.supermartijn642.chunkloaders.ChunkLoaderTile;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.IGuiEventListener;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.client.renderer.WorldVertexBufferUploader;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.math.vector.Matrix4f;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
@@ -26,23 +26,25 @@ import java.util.List;
  */
 public class ChunkLoaderScreen extends Screen {
 
+    private static final ResourceLocation SCREEN_BACKGROUND = new ResourceLocation("chunkloaders", "textures/gui/background.png");
+
     protected World world;
     protected BlockPos pos;
     protected int left, top;
 
-    private final ResourceLocation background;
     private final int backgroundSize;
 
     private boolean doDrag = false;
     private boolean dragState = false;
     private List<ChunkButton> draggedButtons = new ArrayList<>();
 
-    public ChunkLoaderScreen(String type, World world, BlockPos pos, int backgroundSize){
+    public ChunkLoaderScreen(String type, World world, BlockPos pos){
         super(new TranslationTextComponent("block.chunkloaders." + type));
         this.world = world;
         this.pos = pos;
-        this.background = new ResourceLocation("chunkloaders", "textures/gui/" + type + ".png");
-        this.backgroundSize = backgroundSize;
+        ChunkLoaderTile tile = this.getTileOrClose();
+        int gridSize = tile == null ? 1 : tile.getGridSize();
+        this.backgroundSize = gridSize * 15 + (gridSize - 1) + 16;
     }
 
     @Override
@@ -66,7 +68,7 @@ public class ChunkLoaderScreen extends Screen {
     @Override
     public void render(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks){
         this.renderBackground(matrixStack);
-        this.drawBackgroundLayer(partialTicks, mouseX, mouseY);
+        this.drawBackgroundLayer(matrixStack, partialTicks, mouseX, mouseY);
         super.render(matrixStack, mouseX, mouseY, partialTicks);
 
         ChunkLoaderTile tile = this.getTileOrClose();
@@ -79,15 +81,12 @@ public class ChunkLoaderScreen extends Screen {
         });
     }
 
-    private void drawBackgroundLayer(float partialTicks, int mouseX, int mouseY){
+    private void drawBackgroundLayer(MatrixStack matrixStack, float partialTicks, int mouseX, int mouseY){
         ChunkLoaderTile tile = this.getTileOrClose();
         if(tile == null)
             return;
 
-        RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
-
-        Minecraft.getInstance().getTextureManager().bindTexture(this.background);
-        this.drawTexture(this.left, this.top, this.backgroundSize, this.backgroundSize);
+        drawScreenBackground(matrixStack, this.left, this.top, this.backgroundSize, this.backgroundSize);
     }
 
     public void renderToolTip(MatrixStack matrixStack, boolean translate, String string, int x, int y){
@@ -153,16 +152,34 @@ public class ChunkLoaderScreen extends Screen {
         return false;
     }
 
-    private void drawTexture(int x, int y, int width, int height){
-        BufferBuilder bufferbuilder = Tessellator.getInstance().getBuffer();
-        bufferbuilder.begin(7, DefaultVertexFormats.POSITION_TEX);
-        int z = this.getBlitOffset();
-        bufferbuilder.pos(x, y + height, z).tex(1, 0).endVertex();
-        bufferbuilder.pos(x + width, y + height, z).tex(1, 1).endVertex();
-        bufferbuilder.pos(x + width, y, z).tex(0, 1).endVertex();
-        bufferbuilder.pos(x, y, z).tex(0, 0).endVertex();
-        bufferbuilder.finishDrawing();
-        RenderSystem.enableAlphaTest();
-        WorldVertexBufferUploader.draw(bufferbuilder);
+    public static void drawScreenBackground(MatrixStack matrixStack, float x, float y, float width, float height){
+        Minecraft.getInstance().textureManager.bindTexture(SCREEN_BACKGROUND);
+        // corners
+        drawTexture(matrixStack, x, y, 4, 4, 0, 0, 4 / 9f, 4 / 9f);
+        drawTexture(matrixStack, x + width - 4, y, 4, 4, 5 / 9f, 0, 4 / 9f, 4 / 9f);
+        drawTexture(matrixStack, x + width - 4, y + height - 4, 4, 4, 5 / 9f, 5 / 9f, 4 / 9f, 4 / 9f);
+        drawTexture(matrixStack, x, y + height - 4, 4, 4, 0, 5 / 9f, 4 / 9f, 4 / 9f);
+        // edges
+        drawTexture(matrixStack, x + 4, y, width - 8, 4, 4 / 9f, 0, 1 / 9f, 4 / 9f);
+        drawTexture(matrixStack, x + 4, y + height - 4, width - 8, 4, 4 / 9f, 5 / 9f, 1 / 9f, 4 / 9f);
+        drawTexture(matrixStack, x, y + 4, 4, height - 8, 0, 4 / 9f, 4 / 9f, 1 / 9f);
+        drawTexture(matrixStack, x + width - 4, y + 4, 4, height - 8, 5 / 9f, 4 / 9f, 4 / 9f, 1 / 9f);
+        // center
+        drawTexture(matrixStack, x + 4, y + 4, width - 8, height - 8, 4 / 9f, 4 / 9f, 1 / 9f, 1 / 9f);
     }
+
+    public static void drawTexture(MatrixStack matrixStack, float x, float y, float width, float height, float tx, float ty, float twidth, float theight){
+        GlStateManager.color4f(1, 1, 1, 1);
+
+        Matrix4f matrix = matrixStack.getLast().getMatrix();
+        Tessellator tessellator = Tessellator.getInstance();
+        BufferBuilder buffer = tessellator.getBuffer();
+        buffer.begin(7, DefaultVertexFormats.POSITION_TEX);
+        buffer.pos(matrix, x, y + height, 0).tex(tx, ty + theight).endVertex();
+        buffer.pos(matrix, x + width, y + height, 0).tex(tx + twidth, ty + theight).endVertex();
+        buffer.pos(matrix, x + width, y, 0).tex(tx + twidth, ty).endVertex();
+        buffer.pos(matrix, x, y, 0).tex(tx, ty).endVertex();
+        tessellator.draw();
+    }
+
 }
