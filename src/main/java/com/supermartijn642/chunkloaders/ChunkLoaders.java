c6c279da905a370a29df11f091f8cd668b54a945
SuperMartijn624 <martijnvandenbrand@outlook.com>
2022-01-23 17:53:13 +0100

    update version

--- src/main/java/com/supermartijn642/chunkloaders/ChunkLoaders.java
+++ src/main/java/com/supermartijn642/chunkloaders/ChunkLoaders.java
@@ -22,8 +22,8 @@ public class ChunkLoaders {
 
     public static final String MODID = "chunkloaders";
     public static final String NAME = "Chunk Loaders";
-    public static final String VERSION = "1.1.8";
-    public static final String DEPENDENCIES = "required-after:supermartijn642configlib@[1.0.5,)";
+    public static final String VERSION = "1.1.9";
+    public static final String DEPENDENCIES = "required-after:supermartijn642configlib@[1.0.9,)";
 
     public static ChunkLoaders instance;
 
