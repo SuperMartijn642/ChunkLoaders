d979ca3de9fbb01d47a672a82a71e4b52b2752b7
SuperMartijn624 <martijnvandenbrand@outlook.com>
2023-07-31 19:00:44 +0200

    Always display shift key instead of crouch keybind for chunk overwrites in ChunkGridCell

--- src/main/java/com/supermartijn642/chunkloaders/screen/ChunkGridCell.java
+++ src/main/java/com/supermartijn642/chunkloaders/screen/ChunkGridCell.java
@@ -148,7 +148,7 @@ public class ChunkGridCell extends BaseWidget {
                 tooltips.add(canToggleChunk ? 1 : 0, TextComponents.translation("chunkloaders.gui.chunk.others").color(ChatFormatting.WHITE).get());
             if(!ClientUtils.getPlayer().getUUID().equals(this.player) && ClientUtils.getPlayer().hasPermissions(2) && !Screen.hasShiftDown()
                 && (this.isWithinRange.apply(0, 0) || this.isLoaded.apply(0, 0))){
-                Component keyName = TextComponents.fromTextComponent(ClientUtils.getMinecraft().options.keyShift.getTranslatedKeyMessage()).color(ChatFormatting.GOLD).get();
+                Component keyName = TextComponents.translation("key.keyboard.left.shift").color(ChatFormatting.GOLD).get();
                 tooltips.add(TextComponents.translation("chunkloaders.gui.chunk.overwrite", keyName).color(ChatFormatting.WHITE).get());
             }
             tooltips.forEach(tooltipConsumer);
