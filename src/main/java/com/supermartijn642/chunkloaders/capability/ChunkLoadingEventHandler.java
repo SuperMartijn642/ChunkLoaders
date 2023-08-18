1dc20093be2557e479433410b6670674bb73bd5f
SuperMartijn624 <martijnvandenbrand@outlook.com>
2023-08-18 13:40:51 +0200

    Send chunk loader capability data when a player respawns #99

--- src/main/java/com/supermartijn642/chunkloaders/capability/ChunkLoadingEventHandler.java
+++ src/main/java/com/supermartijn642/chunkloaders/capability/ChunkLoadingEventHandler.java
@@ -3,6 +3,7 @@ package com.supermartijn642.chunkloaders.capability;
 import com.supermartijn642.chunkloaders.ChunkLoaders;
 import com.supermartijn642.chunkloaders.packet.PacketFullCapabilityData;
 import net.fabricmc.fabric.api.entity.event.v1.ServerEntityWorldChangeEvents;
+import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
@@ -15,6 +16,7 @@ public class ChunkLoadingEventHandler {
     public static void registerCallbacks(){
         ServerEntityWorldChangeEvents.AFTER_PLAYER_CHANGE_WORLD.register((player, origin, destination) -> onPlayerEnterLevel(player));
         ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> onPlayerJoin(handler.player));
+        ServerPlayerEvents.AFTER_RESPAWN.register((oldPlayer, newPlayer, alive) -> onPlayerEnterLevel(newPlayer));
     }
 
     public static void onPlayerEnterLevel(Player player){
