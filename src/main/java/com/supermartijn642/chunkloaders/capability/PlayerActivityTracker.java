3ed6c00b04b815f72903a36a80f17d9802d3f708
EastRane <28537689+EastRane@users.noreply.github.com>
2025-07-31 00:37:08 +0300

    Read lastActiveTimePerPlayer from an NBT file as well (#153)

--- src/main/java/com/supermartijn642/chunkloaders/capability/PlayerActivityTracker.java
+++ src/main/java/com/supermartijn642/chunkloaders/capability/PlayerActivityTracker.java
@@ -183,6 +183,7 @@ public class PlayerActivityTracker {
             ActiveTime activeTime = new ActiveTime(timeTag.getUniqueId("player"), timeTag.getLong("time"));
             activePlayers.add(activeTime.player);
             sortedActiveTimes.add(activeTime);
+            lastActiveTimePerPlayer.put(activeTime.player, activeTime);
         }
     }
 
