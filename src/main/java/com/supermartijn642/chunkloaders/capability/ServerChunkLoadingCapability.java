3b2c8c3b6aa9a7a60c4408fd2a2a03623c6c99b6
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-07-31 15:12:56 +0200

    Send players a message when their chunk loaders were disabled due to inactivity

--- src/main/java/com/supermartijn642/chunkloaders/capability/ServerChunkLoadingCapability.java
+++ src/main/java/com/supermartijn642/chunkloaders/capability/ServerChunkLoadingCapability.java
@@ -144,7 +144,7 @@ public class ServerChunkLoadingCapability extends ChunkLoadingCapability {
     /**
      * Called when a player becomes active/inactive
      */
-    public void togglePlayerActivity(UUID player, boolean active){
+    public boolean togglePlayerActivity(UUID player, boolean active){
         Set<ChunkPos> chunks = this.loadedChunksPerPlayer.get(player);
         if(chunks != null){
             if(active){
@@ -177,7 +177,9 @@ public class ServerChunkLoadingCapability extends ChunkLoadingCapability {
             }
 
             this.sendToAllPlayers(new PackedTogglePlayerActivity(player, active));
+            return true;
         }
+        return false;
     }
 
     public Set<ChunkPos> getChunksToBeTicked(){
